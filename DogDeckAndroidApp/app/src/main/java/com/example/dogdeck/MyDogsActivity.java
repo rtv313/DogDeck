d64e1b040332ec7fbe96eb24abccbb3c4b295608
rtv313isc <rtv313.isc@hotmail.com>
2020-05-19 22:09:23 -0500

    Add button basic

--- DogDeckAndroidApp/app/src/main/java/com/example/dogdeck/MyDogsActivity.java
+++ DogDeckAndroidApp/app/src/main/java/com/example/dogdeck/MyDogsActivity.java
@@ -1,16 +1,45 @@
 package com.example.dogdeck;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import static android.view.MotionEvent.*;
 
 public class MyDogsActivity extends AppCompatActivity {
 
+    FloatingActionButton addDogFab;
+
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_dogs);
+        addDogFab = (FloatingActionButton) findViewById(R.id.add_dog);
+
+        addDogFab.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if(event.getAction() == MotionEvent.ACTION_DOWN) {
+                    addDogFab.setBackgroundTintList(ContextCompat.getColorStateList(MyDogsActivity.this, R.color.redLight));
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    Toast.makeText(MyDogsActivity.this,"Hiciste Clic",Toast.LENGTH_SHORT).show();
+                    addDogFab.setBackgroundTintList(ContextCompat.getColorStateList(MyDogsActivity.this, R.color.red));
+                }
+                return true;
+            }
+        });
     }
 }
