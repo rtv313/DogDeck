9240667fb1c05932dc3513a2068d9d329f8973a3
rtv313isc <rtv313.isc@hotmail.com>
2020-05-24 13:00:02 -0500

    neural network works

--- DogDeckAndroidApp/app/src/main/java/com/example/dogdeck/DogAnalysis.java
+++ DogDeckAndroidApp/app/src/main/java/com/example/dogdeck/DogAnalysis.java
@@ -4,8 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
+
+import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.Interpreter;
+import org.tensorflow.lite.support.common.FileUtil;
+import org.tensorflow.lite.support.image.ImageProcessor;
+import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
+import org.tensorflow.lite.support.model.Model;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 
 public class DogAnalysis extends AppCompatActivity {
 
@@ -16,6 +31,38 @@ public class DogAnalysis extends AppCompatActivity {
         setContentView(R.layout.activity_dog_analysis);
         dogPhoto = findViewById(R.id.dogPhoto);
         setDogPhoto();
+
+        ImageProcessor imageProcessor = new ImageProcessor.Builder()
+                .add(new ResizeOp(224, 224, ResizeOp.ResizeMethod.BILINEAR))
+                .build();
+
+        Bitmap bitmap = ((BitmapDrawable)dogPhoto.getDrawable()).getBitmap();
+        TensorImage tImage = new TensorImage(DataType.FLOAT32);
+        tImage.load(bitmap);
+        tImage = imageProcessor.process(tImage);
+
+        //TensorBuffer probabilityBuffer = TensorBuffer.createFixedSize(new int[]{120}, DataType.UINT8);
+        float[][] probabilityBuffer = new float[1][120];
+        Interpreter tflite = null;
+
+        try{
+            MappedByteBuffer tfliteModel = FileUtil.loadMappedFile(this, "tf_model.tflite");
+            tflite = new Interpreter(tfliteModel);
+        } catch (IOException e){
+            Log.e("tfliteSupport", "Error reading model", e);
+        }
+
+        // Running inference
+        if(null != tflite) {
+           // tflite.run(tImage.getBuffer(), probabilityBuffer.getBuffer());
+            ByteBuffer buffer =  tImage.getBuffer();
+            tflite.run(buffer, probabilityBuffer);
+    }
+
+        //float [] results = probabilityBuffer.getFloatArray();
+        float [] inferredValue = probabilityBuffer[0];
+        int x = 0;
+
     }
 
     private void setDogPhoto(){
@@ -24,4 +71,5 @@ public class DogAnalysis extends AppCompatActivity {
         Bitmap imageCameraBitmap = BitmapFactory.decodeFile(uri);
         dogPhoto.setImageBitmap(imageCameraBitmap);
     }
+
 }
